a2c41a3cfbfbe6542ef062be13b9ded5b5ce6f69
Reece Bedding <reecebedding@hotmail.co.uk>
2018-01-19 22:48:01 +0000

    Add active link highlighting in sidebar

--- src/budjit.ui/ClientApp/app/app.module.ts
+++ src/budjit.ui/ClientApp/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { TransactionList } from "./transactions/transactionList.component";
-import { DataService } from "./shared/dataService";
-import { RouterModule } from "@angular/router";
+import { SideNav } from "./shared/navigation/sidenav.component";
 import { Dashboard } from "./dashboard/dashboard.component";
 
-let routes = [
-  { path: "", component: Dashboard }
+import { DataService } from "./shared/dataService";
+
+
+let routes: Routes = [
+  { path: "", redirectTo:"/dashboard", pathMatch:"full" },
+  { path: "dashboard", component: Dashboard },
+  { path: "upload", component: Dashboard },
+  { path: "new-tag", component: Dashboard },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     TransactionList,
+    SideNav,
     Dashboard
   ],
   imports: [
---
+++ src/budjit.ui/ClientApp/app/shared/navigation/sidenav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "side-nav",
+    templateUrl: "sidenav.component.html",
+    styleUrls: []
+})
+
+export class SideNav {
+
+}
